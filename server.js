5a49c5dde944eab6d0aecab6add361ffcfeefa4c
Steven Ly <ly.steven16@gmail.com>
2021-02-24 20:30:10 -0600

    Added routes to serve html pages

--- server.js
+++ server.js
@@ -6,10 +6,12 @@ const { animals } = require('./data/animals');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+app.use(express.static('public'));
 // parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
+
 
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
@@ -111,11 +113,29 @@ app.post('/api/animals', (req, res) => {
     }
 });
 
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
 });
 
 // https://agile-inlet-41097.herokuapp.com/api/animals 
+// http://localhost:3001
 
 
 
